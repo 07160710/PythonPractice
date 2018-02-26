eafe4409f0860dab10d77e62d84c04e759412408
huangkaifeng <1195745286@qq.com>
2018-02-26 13:18:36 +0800

    变量

---
+++ variables/variables_test.py
@@ -0,0 +1,57 @@
+# 变量名 = 值
+''' a = 1
+print(a)
+'''
+a1 = 1
+b2 = 3
+print(a1, b2)
+# 方式2
+# 变量名1，变量名2 = 值1 ， 值2
+a, b = 2, 3
+print(a, b)
+
+# 方式3
+c = 6
+d = 6
+c = d = 3
+print(c,d)
+
+# 6+1,6+2,6+3
+print("----------------------------")
+num = 8
+print(num + 1)
+print(num + 2)
+print(num + 3)
+print(num + 4)
+print(num + 5)
+
+print("----------------------------")
+# 节省空间（磁盘，内存）
+# 字符串拼接
+name ="我是最厉害的"
+print(name + "1")
+print(name + "2")
+print(name + "3")
+print(name + "4")
+
+print("----------------------------")
+#执行顺序从上往下
+age = 10
+# 一个变量只能引用一个数值
+age = 11
+print(age)
+#命名规范
+name = "aaa"
+_name2_ = "aaaaa"
+phoneNum = "111111111"
+# class = 10
+name1 = 'sz'
+Name1 = 'sz1'
+print(name1,Name1)
+''' 字母数字下划线
+ 不能数字开头
+ 见字如意
+ 驼峰标识
+ 非关键字
+ 区分大小写 '''
+
