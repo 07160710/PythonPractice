ee1aa6c745c1544ffc32a3fdca74442b66c51e42
huangkaifeng <1195745286@qq.com>
2018-02-26 20:36:28 +0800

    Python的输入输出

---
+++ shurushuchu/shuru_test.py
@@ -0,0 +1,27 @@
+#-*- coding:utf8 -*-
+# Python 2.x 版本
+# 函数 ： 封装了一些功能，到时候，只需要写一个函数名字，就可以使用这些功能。
+# 未加工的输入
+# content = raw_input("请输入内容")
+# print type(content)
+# print content
+
+# content = input("请输入内容")
+# print type(content)
+# print content
+# content = raw_input("请输入内容：")
+# result = eval(content)
+# print type(result)
+# print result
+
+
+# python 3.x 版本(等价于python 2.x 的raw_input)
+# content = input("请输入内容：")
+# print(type(content))
+# print(content)
+
+# 等价于python 2.x 的input
+# content = input("请输入内容：")
+# result = eval(content)
+# print(type(result))
+# print(result)
