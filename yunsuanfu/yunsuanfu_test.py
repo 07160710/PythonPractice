5e20ef65137190330f4d69fb269c2b4b5a515c5e
huangkaifeng <1195745286@qq.com>
2018-02-26 16:40:41 +0800

    比较运算符，复合运算符

--- yunsuanfu/yunsuanfu_test.py
+++ yunsuanfu/yunsuanfu_test.py
@@ -29,3 +29,49 @@ i = r = s = 11
 #不可除以0，优先级级  （）先使用
 result = (1+2) *3 / 4
 print(result)
+#行列
+# *   *   *   *
+# *   *   *   *
+# *   *
+# num = 6
+# row = num // 4
+# col = num % 4
+# print(row,col)
+# 复合运算符  a x= 值   =》  a = a x 值
+# +=运算符
+num = 10
+#num = num + 5
+num += 5
+print(num)
+# -=运算符  *=  **=
+w = 10
+# w *= 20
+w **= 2  # w = w ** 2
+print(w)
+# *= 运算符
+# /=运算符  //=
+# 比较运算符（>   <   ==  >=   <=  !=   is  ）  <>  等价于 !=
+flag = 10 == 2
+print(flag)
+x = 10
+bs = 10
+# 比较唯一标识
+# id()查看唯一标识码
+print(id(x),id(bs))
+print(x is bs)
+print("--------------------")
+a1 = [1]
+b1 = [1]
+print(a1 == b1)
+print(id(a1),id(b1))
+print(a1 is b1)
+
+
+# 链式比较运算符
+
+nums = 10
+# nums > 5 && nums < 20
+print(5 < nums < 20)
+
+# 逻辑运算符
+
