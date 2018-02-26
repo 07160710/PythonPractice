75cd129a841a9c6f8adc0b1047e7fa0ff5a42c2c
huangkaifeng <1195745286@qq.com>
2018-02-26 21:00:43 +0800

    Python2.x版本的输出

---
+++ shurushuchu/shuchu_test_py2.py
@@ -0,0 +1,29 @@
+# _*_ coding:utf-8 _*_
+# pyhon2.x 输出
+# 输出一个值
+# print 123
+#输出变量
+# num = 10
+# print num
+# 输出多个变量
+# num2 = 66
+# print num , num2
+# 格式化输出
+# name = "kf"
+# age = 20
+# 我的名字是xxx，年龄是xx
+# print "我的名字是" , name , ",年龄是",age
+# newStr = "我的名字是%s,年龄是%d"%(name,age)
+# print newStr
+#找下标0.。。1.。2找位置输出
+# print "我的名字是{0},年龄是{1}".format(name,age)
+# 输出到文件中
+# f = open("test.txt","w")
+# print >>f, "xxxxxxxxxxxxxxxxx"
+# 输出不自动换行
+# print "1",
+# print "2",
+# print "3",
+# 输出的各个数据，使用分隔符分割
+# print "a","-","b","-","c"
+# print "-".join(["a","b","c"])
