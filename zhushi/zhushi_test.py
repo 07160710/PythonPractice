0d5cee3b84fa57487a4473ee8a947582354d6aa5
huangkaifeng <1195745286@qq.com>
2018-02-26 12:37:23 +0800

    Python之路开启

---
+++ zhushi/zhushi_test.py
@@ -0,0 +1,20 @@
+#-*- coding:utf8 -*-
+print(1)
+# print(2)
+print(3)
+# 注释语言
+print(4)
+
+def run(a,b,c,d):
+    print(a,b,c,d)
+
+
+
+# 多行注释
+''' 12345 '''
+"""" abcde
+特殊注释
+Python2默认不支持
+#!/user/bin/python
+# coding=utf-8
+     # encoding=utf-8 """
