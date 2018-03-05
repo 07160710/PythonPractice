c8b95db24c3902eff85d4b94bc256582b49adda4
huangkaifeng <1195745286@qq.com>
2018-03-05 10:15:43 +0800

    Python —if嵌套分支

--- python_if/practice_test.py
+++ python_if/practice_test.py
@@ -19,13 +19,18 @@
 # if score >= 0 and score < 60:
 #     print("不及格")
 
+
+# if 嵌套  优化性能
 score = input("请输入你的分数：")
 score = float(score)
 if 90 <= score <= 100:
     print("优秀")
-if 80 <= score < 90:
-    print("良好")
-if 60 <= score < 80:
-    print("及格")
-if 0 <= score < 60:
-    print("不及格")
+else:
+    if 80 <= score < 90:
+        print("良好")
+    else:
+       if 60 <= score < 80:
+           print("及格")
+       else:
+           if 0 <= score < 60:
+               print("不及格")
