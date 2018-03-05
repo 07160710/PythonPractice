9a8d3cb529d24a0360b2a6b59e4a64fc956bf410
huangkaifeng <1195745286@qq.com>
2018-03-05 10:22:43 +0800

    Python —if多分支

--- python_if/practice_test.py
+++ python_if/practice_test.py
@@ -21,16 +21,28 @@
 
 
 # if 嵌套  优化性能
+# score = input("请输入你的分数：")
+# score = float(score)
+# if 90 <= score <= 100:
+#     print("优秀")
+# else:
+#     if 80 <= score < 90:
+#         print("良好")
+#     else:
+#        if 60 <= score < 80:
+#            print("及格")
+#        else:
+#            if 0 <= score < 60:
+#                print("不及格")
+
+# 多分支   if ...elif.....else.....
 score = input("请输入你的分数：")
 score = float(score)
 if 90 <= score <= 100:
     print("优秀")
-else:
-    if 80 <= score < 90:
-        print("良好")
-    else:
-       if 60 <= score < 80:
-           print("及格")
-       else:
-           if 0 <= score < 60:
-               print("不及格")
+elif 80 <= score < 90:
+    print("良好")
+elif 60 <= score < 80:
+    print("及格")
+elif 0 <= score < 60:
+    print("不及格")
